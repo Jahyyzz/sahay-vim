5b77da9487bf691de18471750c35747ade299d23
sahay <103405100+Jahyyzz@users.noreply.github.com>
2022-07-14 02:11:59 -0300

    Update and rename src.sh to sr.sh

--- sr.sh
+++ sr.sh
@@ -8,3 +8,6 @@ pkg update && pkg upgrade
 pkg install nodejs && npm install -g yarn
 pkg install python && pip install jedi # tenso né amigo kk
 pkg install vim wget curl nano git && pkg install vim-python
+
+echo 'executando o ultimo programa..'
+bash sahay.sh

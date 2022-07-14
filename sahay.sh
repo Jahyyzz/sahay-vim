0a12031b58a094ae59d76dda14b0aab8302feb6f
sahay <103405100+Jahyyzz@users.noreply.github.com>
2022-07-14 02:19:22 -0300

    Create sahay.sh

---
+++ sahay.sh
@@ -0,0 +1,11 @@
+#!usr/bin/bash
+echo 'jahyzao gosto....'
+echo "print('desculpe lkkkk')" > help.py && chmod +x help.py
+touch ~/.vimrc && wget -O ~/.vimrc https://gist.githubusercontent.com/Jahyyzz/8499638acc7d18c4f0a26d165dcd85e1/raw/494aafb79b6f0975fe7fdb7fa3850dd512151aed/sahay-vim
+
+echo 'os scripts acabaram ;-;'
+vim ~/.vimrc
+
+echo 'caso queria fazer alguma alteraçao é so entrar em (vim ~/.vimrc)'
+echo '.'
+echo 'caso vc queira um help, execute o arquivo help.py (python help.py) ou (./help.py)'

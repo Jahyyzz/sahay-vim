e6c6fff2a9959e2cf2e878cc8354d3780104111c
sahay <103405100+Jahyyzz@users.noreply.github.com>
2022-07-14 00:52:40 -0300

    Create new file

---
+++ src.sh
@@ -0,0 +1,10 @@
+#!usr/bin/bash
+pkg install figlet
+
+echo 'sahay-simple ide'
+echo 'fazendo download dos requisitos...'
+
+pkg update && pkg upgrade
+pkg install nodejs && npm install -g yarn
+pkg install python && pip install jedi # tenso né amigo kk
+pkg install vim wget curl nano git && pkg install vim-python
